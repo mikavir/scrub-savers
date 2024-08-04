a29975fc8afb46022b76bcf2b12de867688f11a2
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-08-04 11:19:34 +0000

    Add Procfile and update requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scrub_savers.wsgi:application
--- scrub_savers/settings.py
+++ scrub_savers/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+
 from pathlib import Path
+import dj_database_url
 if os.path.exists("env.py"):
     import env
 
@@ -103,14 +105,21 @@ WSGI_APPLICATION = 'scrub_savers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
