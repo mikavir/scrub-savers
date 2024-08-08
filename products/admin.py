6fbb8e031fc1be0a2356006bee9fc91a6f8884bb
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-08-08 21:48:04 +0000

    Add fixtures and models for products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Product, Category, Profession, Colours
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ColoursAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Profession, ProfessionAdmin)
+admin.site.register(Colours, ColoursAdmin)
