60402db68165f0bce2d62d27319ad43af567a0cc
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-08-26 12:28:42 +0000

    Add product filtering functionality

--- products/views.py
+++ products/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Colours, Profession
 
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
+    categories = None
+    colours = None
+    profession = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
+        if 'colours' in request.GET:
+            colours = request.GET['colours'].split(',')
+            products = products.filter(colours__name__in=colours)
+            colours = Colours.objects.filter(name__in=colours)
+        
+        if 'profession' in request.GET:
+            profession = request.GET['profession'].split(',')
+            products = products.filter(profession__name__in=profession)
+            profession = Profession.objects.filter(name__in=profession)
+        
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,9 +44,14 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_profession': profession,
+        'current_colours': colours,
+
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
