1208dbcbe84c5dfc7c55eaa77c180a1f7d9bb89e
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-08-31 16:31:36 +0000

    Add loading overlay when authenticating

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -49,10 +49,13 @@ card.addEventListener('change', function (event) {
 // Handle form submit
 let form = document.getElementById('payment-form');
 
+
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -66,12 +69,13 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
-                console.log("Submitted")
             }
         }
     });
