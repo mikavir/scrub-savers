c7cf7af39d2db9737c378f4b53ab16c39fd33971
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-01 20:21:45 +0000

    Implement add product view and create template

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category, Profession, Colour
+from .models import Product, Category, Profession, Colours
 
 
 class ProductForm(forms.ModelForm):
@@ -11,7 +11,7 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        colour = Colour.objects.all()
+        colour = Colours.objects.all()
         profession = Profession.objects.all()
 
         # Get friendly names of categories
@@ -20,7 +20,7 @@ class ProductForm(forms.ModelForm):
 
         # Get friendly names of colours 
         colour_friendly_names = [(c.id, c.get_friendly_name()) for c in colour]
-        self.fields['colour'].choices = colour_friendly_names
+        self.fields['colours'].choices = colour_friendly_names
 
         # Get friendly names of profession
         profession_friendly_names = [(p.id, p.get_friendly_name()) for p in profession]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category, Colours, Profession
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -84,3 +85,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
