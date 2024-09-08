45a13d84bea5df0b0629d8f48763a495d5cf0450
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-08 19:07:10 +0000

    Add sender variable to sending confirmation

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,6 +19,7 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
+        sender = settings.DEFAULT_FROM_EMAIL
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
@@ -30,7 +31,7 @@ class StripeWH_Handler:
         send_mail(
             subject,
             body,
-            settings.DEFAULT_FROM_EMAIL,
+            sender,
             [cust_email]
         )        
 
