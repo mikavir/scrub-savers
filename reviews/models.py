15a8025b688b15aca1abb2e3194eb0d98174f857
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-12 12:26:45 +0000

    Set a max value of 5 to ratings

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from products.models import Product
 from profiles.models import UserProfile
@@ -12,5 +13,9 @@ class ProductReview(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reviews')
     content = models.TextField(blank=True, null=True)
-    rating = models.IntegerField()
+    # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model/12026867#12026867
+    rating = models.IntegerField(default=1,validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
     date_added = models.DateTimeField(default=timezone.now, editable=False)
