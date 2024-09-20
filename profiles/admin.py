a6300d74d19ecd470059f6e4b658516786e62cae
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-20 17:57:16 +0000

    Add profiles to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
 # Register your models here.
+class ProfilesAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+    )
+
+admin.site.register(UserProfile, ProfilesAdmin)
