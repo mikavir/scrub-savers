bd94ab1d103c6f3fefa70b7340b777563d3c15b7
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-20 17:56:55 +0000

    Add subject to reviews models

--- reviews/models.py
+++ reviews/models.py
@@ -12,6 +12,7 @@ class ProductReview(models.Model):
         verbose_name_plural = 'Product Reviews'
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reviews')
+    subject = models.TextField(blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model/12026867#12026867
     rating = models.IntegerField(default=1,validators=[
