c38973f90941bde5b476e0fd757acee28ef9bf9c
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-21 17:29:53 +0000

    Add success feedback when user emails

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 def contact(request):
-    """ Renders the contact page and allow users to send message """
+    """ Renders the contact page and allow users to send message 
+    https://mailtrap.io/blog/django-contact-form/ """
     """ A view to return the contact page """
     form = ContactForm()
 
@@ -19,13 +21,17 @@ def contact(request):
         body = render_to_string(
             'contact/auto_reply/email_body.txt',
             {'name': name})
-        
-        send_mail(
-            subject,
-            body,
-            sender,
-            [cust_email]
-        )   
+        try:
+            send_mail(
+                subject,
+                body,
+                sender,
+                [cust_email]
+            )
+            return redirect("success")
+        except Exception as e:
+            # Log the error for debugging purposes
+            messages.error(request, (f"Failed to send email to {cust_email}. Error: {str(e)}"))
     
     template = 'contact/contact.html'
     context = {
@@ -33,3 +39,8 @@ def contact(request):
     }
 
     return render(request, template, context)
+
+
+def success(request):
+    """ Renders when user have successfuly sent an email"""
+    return render(request, 'contact/success.html')
