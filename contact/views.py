3c993b5406cd27548830ba2c424bcb3fea23e2a2
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-21 17:19:52 +0000

    Add auto reply mail when user contacts

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 # Create your views here.
 def contact(request):
+    """ Renders the contact page and allow users to send message """
     """ A view to return the contact page """
+    form = ContactForm()
 
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        sender = settings.DEFAULT_FROM_EMAIL
+        cust_email = request.POST.get('email')
+        name = request.POST.get('name')
+        subject = "Thank you for reaching out!"
+        body = render_to_string(
+            'contact/auto_reply/email_body.txt',
+            {'name': name})
+        
+        send_mail(
+            subject,
+            body,
+            sender,
+            [cust_email]
+        )   
+    
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
