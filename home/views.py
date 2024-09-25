b35654a50aa0aecc8293ac94ee47f5aa99b7931a
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-09-25 11:26:34 +0000

    Add recommendations section

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
+import random
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    products = list(Product.objects.all())
 
-    return render(request, 'home/index.html')
+    products = random.sample(products, 4)
+
+    return render(request, 'home/index.html', {'products': products})
