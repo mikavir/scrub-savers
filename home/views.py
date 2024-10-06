dae485e9dea99aa811af646abc14254358f28a2b
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-10-06 21:04:21 +0000

    Fix flake8 issues in home, remove unused files

--- home/views.py
+++ home/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 from products.models import Product
 import random
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
     products = list(Product.objects.all())
 
-    if len(products) > 4: 
+    if len(products) > 4:
         products = random.sample(products, 4)
-
 
     return render(request, 'home/index.html', {'products': products})
