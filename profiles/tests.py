192915a526f91b324025dac10f15b9ce041f7201
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-10-07 12:39:34 +0000

    Test views on profile page

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,23 @@
+import os
+if os.path.exists("env.py"):
+    import env  # noqa
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+
+
+password = os.environ.get("TEST_PASSWORD")
+email = os.environ.get("TEST_EMAIL")
 
 # Create your tests here.
+class TestViews(TestCase):
+
+    def test_profile_page(self):
+        """ Test views on profile page """
+        # Log user in 
+        self.client = Client()
+        user = User.objects.create_user('test_admin', password)
+        self.client.force_login(user=user)
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
