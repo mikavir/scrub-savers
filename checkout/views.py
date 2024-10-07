2e1007d733efff11d67b9a16a591616cc32f04ce
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-10-07 10:20:52 +0000

    Fix flake8 errors in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -72,16 +74,21 @@ def checkout(request):
                         # save products into profile.user purchases:
                         if request.user.is_authenticated:
                             try:
-                                profile = UserProfile.objects.get(user=request.user)
+                                profile = UserProfile.objects.get(
+                                    user=request.user
+                                    )
                                 # https://stackoverflow.com/questions/1182380/how-to-add-data-into-manytomany-field
                                 # https://docs.djangoproject.com/en/5.1/topics/db/queries/#:~:text=To%20save%20changes%20to%20an,the%20database%2C%20use%20save()%20.&text=This%20performs%20an%20UPDATE%20SQL,you%20explicitly%20call%20save()%20.
                                 profile.user_purchases.add(product)
                                 profile.save()
                             except UserProfile.DoesNotExist:
-                                messages.error(request, (" You do not have a profile"))
-                    
+                                messages.error(
+                                    request,
+                                    (" You do not have a profile")
+                                )
+
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data['items_by_size'].items(): # noqa
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -91,30 +98,39 @@ def checkout(request):
                             order_line_item.save()
                             if request.user.is_authenticated:
                                 try:
-                                    profile = UserProfile.objects.get(user=request.user)
+                                    profile = UserProfile.objects.get(
+                                        user=request.user
+                                        )
                                     profile.user_purchases.add(product)
                                     profile.save()
                                 except UserProfile.DoesNotExist:
-                                    messages.error(request, (" You do not have a profile"))
-                            
+                                    messages.error(
+                                        request,
+                                        (" You do not have a profile")
+                                    )
+
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
-                        "Please call us for assistance!")
+                        "One item in your bag wasn't found. Please contact us!") # noqa
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(
+                reverse('checkout_success', args=[order.order_number])
+            )
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(
+                request,
+                "There's nothing in your bag at the moment"
+            )
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
@@ -126,7 +142,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form with any info the
+        # user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
