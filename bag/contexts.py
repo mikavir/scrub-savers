dc03085ae90c206a849b6a1dad2db9a9ce1dc148
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-10-07 10:36:49 +0000

    Resolve flake8 issues and remove files in bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
+    """ Track the quantity of items in the bag contents
+        for posting to the database."""
 
     bag_items = []
     total = 0
@@ -38,9 +41,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
