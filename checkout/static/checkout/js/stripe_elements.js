60050cde3c1373d94781d5f683219277b85d8d7e
Mikaela Virtucio <mikavirtucio@gmail.com>
2024-10-12 20:35:20 +0000

    Add missing semi-colon

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 11, jquery: true */
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
@@ -118,5 +119,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
